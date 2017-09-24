5a5ad3001655ef11694d65101379a20dcb9e3863
Vic-L <vljc17@gmail.com>
2017-09-24 17:37:17 +0800

    feat: setup routing in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React from "react"
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Content from '_components/Content'
+import Home from '_components/Home'
 
 const App = () => {
-  return (~
-    .grid-container.fluid
-      .grid-x.grid-margin-x
-        .cell.small-4 App
-        .cell.small-4 App
-        .cell.small-4 App
-  ~)
+  return (
+    <main className='grid-container'>
+      <div className='grid-x grid-margin-x'>
+        <Switch>
+          <Route
+            path="/:contentId"
+            component={Content} />
+          <Route
+            path="/"
+            exact
+            component={Home} />
+        </Switch>
+      </div>
+    </main>
+  )
 }
 
 export default App
