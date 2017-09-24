39e1a9b526b7ea1a1ecca1ae838c3b0632c9415c
Vic-L <vljc17@gmail.com>
2017-09-24 17:52:31 +0800

    Revert "Revert "refactor: convert to haml""
    This reverts commit 0e07675d280412fc9664497c06761ec25ae89ccd.

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,18 @@ import Content from '_components/Content'
 import Home from '_components/Home'
 
 const App = () => {
-  return (
-    <main className='grid-container'>
-      <div className='grid-x grid-margin-x'>
-        <Switch>
-          <Route
+  return (~
+    %main.grid-container
+      .grid-x.grid-margin-x
+        %Switch
+          %Route(
             path="/:contentId"
-            component={Content} />
-          <Route
+            component={Content})
+          %Route(
             path="/"
-            exact
-            component={Home} />
-        </Switch>
-      </div>
-    </main>
-  )
+            exact=""
+            component={Home})
+  ~)
 }
 
 export default App
